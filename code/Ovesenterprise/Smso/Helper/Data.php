583d1051b00d36c5d922e04c2f505f0629a174b1
Lillik <lilian.codreanu@gmail.com>
2020-02-05 13:44:10 +0200

    Initial commit

---
+++ code/Ovesenterprise/Smso/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category   Smso
+ * @package    Ovesenterprise_Smso
+ * @copyright  Copyright (c) 2020 Smso (https://www.smso.ro/)
+ * @author     Lilian Codreanu <lilian.codreanu@ovesenterprise.com>
+ */
+
+class Ovesenterprise_Smso_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
