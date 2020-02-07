688d700116b9e011a073d559fa2b589860d51105
Lillik <lilian.codreanu@gmail.com>
2020-02-07 09:45:52 +0200

    Add config Sender source

---
+++ code/Ovesenterprise/Smso/Model/System/Config/Source/Sender.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @category   Smso
+ * @package    Ovesenterprise_Smso
+ * @copyright  Copyright (c) 2020 Smso (https://www.smso.ro/)
+ * @author     Lilian Codreanu <lilian.codreanu@ovesenterprise.com>
+ */
+
+class Ovesenterprise_Smso_Model_System_Config_Source_Sender
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options = [];
+
+        return $options;
+    }
+}
